620683daed7c0b2d35370b0d19a99f5aa48e9dce
George MacKay-Shore <george.mackay-shore@and.digital>
2020-06-18 20:47:46 +0100

    Clean up app component and fix compile warnings

--- src/Components/PlayerIdentifier/PlayerIdentifier.js
+++ src/Components/PlayerIdentifier/PlayerIdentifier.js
@@ -2,12 +2,12 @@ import React from 'react';
 import css from './PlayerIdentifier.module.css';
 import image from '../../logo512.png';
 
-const PlayerIdentifier = ({ name, icon }) => {
+const PlayerIdentifier = ({ name }) => {
   return (
     <>
       <div className={css.playerCard}>
         <div className={css.iconContainer}>
-          <img className={css.icon} src={image}></img>
+          <img className={css.icon} src={image} alt="Player Avatar" />
         </div>
         <div className={css.playerName}>{name}</div>
       </div>
