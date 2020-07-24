4060671ea0c8ae13fbcbf451133e8dfe4e5995d2
George MacKay-Shore <mangohatface@bigfatsodacompany.com>
2020-07-24 21:17:56 +0100

    Refactor RaceCard to have single responsibility

--- src/Components/RaceCard/RaceCard.js
+++ src/Components/RaceCard/RaceCard.js
@@ -11,7 +11,6 @@ import Checkbox from '@material-ui/core/Checkbox';
 
 import { makeStyles } from '@material-ui/core/styles';
 
-import AddCircleIcon from '@material-ui/icons/AddCircleOutlineTwoTone';
 import { TableFooter } from '@material-ui/core';
 
 const useStyles = makeStyles({
@@ -33,50 +32,44 @@ const useStyles = makeStyles({
 
 const RaceCard = ({ raceCard }) => {
   const classes = useStyles();
+  const horses = raceCard.horses;
 
-  if (raceCard) {
-    const horses = raceCard.horses;
-
-    return (
-      <div className={css.raceCard}>
-        <div>{raceCard.name}</div>
-        <TableContainer component={Paper}>
-          <Table className={classes.table} aria-label="simple table">
-            <TableHead className={classes.head}>
-              <TableRow>
-                <TableCell>Horse No.</TableCell>
-                <TableCell>Horse Name</TableCell>
-                <TableCell>Odds</TableCell>
-                <TableCell>Winner</TableCell>
+  return (
+    <div className={css.raceCard}>
+      <div>{raceCard.name}</div>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead className={classes.head}>
+            <TableRow>
+              <TableCell>Horse No.</TableCell>
+              <TableCell>Horse Name</TableCell>
+              <TableCell>Odds</TableCell>
+              <TableCell>Winner</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {horses.map((horse, key) => (
+              <TableRow key={key}>
+                <TableCell component="th" scope="row">
+                  {horse.number}
+                </TableCell>
+                <TableCell>{horse.name}</TableCell>
+                <TableCell>{horse.odds}</TableCell>
+                <TableCell align="center">
+                  <Checkbox checked={horse.winner} />
+                </TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {horses.map((horse, key) => (
-                <TableRow key={key}>
-                  <TableCell component="th" scope="row">
-                    {horse.number}
-                  </TableCell>
-                  <TableCell>{horse.name}</TableCell>
-                  <TableCell>{horse.odds}</TableCell>
-                  <TableCell align="center">
-                    <Checkbox checked={horse.winner} />
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-          <TableFooter>{raceCard.link}</TableFooter>
-        </TableContainer>
-      </div>
-    );
-  } else {
-    return (
-      <div className={css.emptyRacecard}>
-        <AddCircleIcon fontSize="large" />
-        Add Race Card
-      </div>
-    );
-  }
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell>{raceCard.link}</TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 };
 
 export default RaceCard;
