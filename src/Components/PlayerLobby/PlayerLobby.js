86ed9bcdce1e393411697bfbb4ff702251628c5c
Phil Steer <pd@incb.us>
2020-08-12 10:18:50 +0100

    Re-indent, some re-format, add a success/error alert to 'place bets'

--- src/Components/PlayerLobby/PlayerLobby.js
+++ src/Components/PlayerLobby/PlayerLobby.js
@@ -1,166 +1,190 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import Body from '../Body/Body';
 import { useHistory, useLocation } from 'react-router-dom';
-
+import { ToastContainer } from 'react-toastr';
 import Card from '@material-ui/core/Card';
-
 import { makeStyles } from '@material-ui/core/styles';
+
+import Body from '../Body/Body';
+import useApiGetResult from "../../hooks/useLoading";
+import WagerCard from '../WagerCard/WagerCard';
+import { postToApi } from '../../utils/apiLayer';
+import { useWebsocket } from "../../hooks/useWebsocket";
 import css from './PlayerLobby.module.css';
 
-import useApiGetResult from "../../hooks/useLoading";
-import { postToApi } from '../../utils/apiLayer';
-import WagerCard from '../WagerCard/WagerCard';
-import { useWebsocket } from "../../hooks/useWebsocket";
-
 const useStyles = makeStyles({
-  root: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 120,
-    height: 80,
-    border: '1px solid #00000026'
-  }
+    root: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 120,
+        height: 80,
+        border: '1px solid #00000026'
+    }
 });
 
 const PlayerLobby = () => {
-  const classes = useStyles();
-  const history = useHistory();
-  const { state = {} } = useLocation();
-  const { sessionInfo } = state;
-  const { raceDayId } = sessionInfo;
+    let toastrContainer;
+    const classes = useStyles();
+    const history = useHistory();
+    const { state = {} } = useLocation();
+    const { sessionInfo } = state;
+    const { raceDayId } = sessionInfo;
+    const raceDayEndpoint = `/raceday/${raceDayId}`;
 
-  const [isLoading, setIsLoading] = useState(true);
-  const [apiError, setApiError] = useState({ error: false, messages: [] });
+    const [isLoading, setIsLoading] = useState(true);
+    const [apiError, setApiError] = useState({ error: false, messages: [] });
+    const [raceDayData, isRaceDataLoading, raceDataApiError] = useApiGetResult({ races: [] }, raceDayEndpoint);
+    const [playerData, isPlayerDataLoading, playerDataApiError] = useApiGetResult([], `${raceDayEndpoint}/leaderboard`);
+    const [placeBetText, setPlaceBetText] = useState('Place Bet');
+    const [currentPlayerBalance, setCurrentPlayerBalance] = useState(0);
+    const [advanceToRace] = useWebsocket('/eventstate');
 
-  const raceDayEndpoint = `/raceday/${raceDayId}`;
+    useEffect(() => {
+        if (advanceToRace === 'started') {
+            history.push('/Race',
+            {
+                race: getNextRace(),
+                isLastRace: isLastRace(),
+                sessionInfo
+            });
+        }
+    }, [advanceToRace])
 
-  const [raceDayData, isRaceDataLoading, raceDataApiError] = useApiGetResult({ races: [] }, raceDayEndpoint);
-  const [playerData, isPlayerDataLoading, playerDataApiError] = useApiGetResult([], `${raceDayEndpoint}/leaderboard`);
-  const [placeBetText, setPlaceBetText] = useState('Place Bet');
-  const [currentPlayerBalance, setCurrentPlayerBalance] = useState(0);
-
-  const [advanceToRace] = useWebsocket('/eventstate');
-
-  useEffect(() => {
-    if (advanceToRace === 'started') {
-      history.push('/Race',
-        {
-          race: getNextRace(),
-          isLastRace: isLastRace(),
-          sessionInfo
+    const getNextRace = () => {
+        return raceDayData.races.find((race) => {
+            return race.state === "not-started"
         });
     }
-  }, [advanceToRace])
 
+    useEffect(() => {
+        setIsLoading(isRaceDataLoading || isPlayerDataLoading);
+    }, [isRaceDataLoading, isPlayerDataLoading]);
 
+    useEffect(() => {
+        const messages = [raceDataApiError, playerDataApiError].filter(message => message);
+        setApiError({
+            error: !!raceDataApiError && !!playerDataApiError,
+            messages,
+        });
+    }, [raceDataApiError, playerDataApiError]);
 
-  const getNextRace = () => {
-    return raceDayData.races.find((race) => {
-      return race.state === "not-started"
-    });
-  }
+    const isLastRace = () => {
+        return (raceDayData.races.filter(race => {
+            return race.state === 'not-started'
+        }).length === 1)
+    }
 
-  useEffect(() => {
-    setIsLoading(isRaceDataLoading || isPlayerDataLoading);
-  }, [isRaceDataLoading, isPlayerDataLoading]);
+    const handleBetsChanged = () => {
+        const wagers = getWagers();
 
-  useEffect(() => {
-    const messages = [raceDataApiError, playerDataApiError].filter(message => message);
-    setApiError({
-      error: !!raceDataApiError && !!playerDataApiError,
-      messages,
-    });
-  }, [raceDataApiError, playerDataApiError]);
-  
-  const isLastRace = () => {
-    return (raceDayData.races.filter(race => {
-      return race.state === 'not-started'
-    }).length === 1)
-  }
+        //Replace with the current balance of the player
+        //when this feature has been implemented.
+        let currentBalance = parseFloat(raceDayData.initialStake);
+        let wagerValues = aggregateWagerValues(wagers);
 
-  
-  const handleBetsChanged = () => {
-    const wagers = getWagers();
+        setCurrentPlayerBalance(currentBalance - wagerValues);
+    }
 
-    //Replace with the current balance of the player when this feature has been implemented.
-    let currentBalance = parseFloat(raceDayData.initialStake);
-    let wagerValues = aggregateWagerValues(wagers);
+    const getWagers = () => {
+        const raceCard = getNextRace();
 
-    setCurrentPlayerBalance(currentBalance - wagerValues);
-  }
+        return raceCard.horses.filter(horse => {
+            return (horse.bet);
+        }).map(horse => {
+            return {
+                player: sessionInfo.playerId,
+                race: raceCard._id,
+                horseNumber: horse.number,
+                amount: horse.bet
+            }
+        });
+    }
 
-  const getWagers = () => {
-    const raceCard = getNextRace();
+    const handlePlaceBet = async (props) => {
+        const wagers = getWagers();
 
-    return raceCard.horses.filter(horse => {
-      return (horse.bet);
-    }).map(horse => {
-      return {
-        player: sessionInfo.playerId,
-        race: raceCard._id,
-        horseNumber: horse.number,
-        amount: horse.bet
-      }
-    });
-  }
+        function onSuccess(wagers) {
+            setPlaceBetText('Update Bet');
+            alert('it worked');
+        }
 
-  const handlePlaceBet = async () => {
-     setPlaceBetText('Update Bet');
-    const wagers = getWagers();
+        function onError() {
+            setPlaceBetText('Place Bet');
+            alert('it failed');
+        }
 
-    await postToApi('/wagers', {
-      player: sessionInfo.playerId,
-      wagers: wagers
-    });
-  };
+        const response = await postToApi('/wagers', {
+            player: sessionInfo.playerId,
+            wagers: wagers
+        });
 
-  const aggregateWagerValues = (wagers) => wagers.reduce((accum, item) => parseFloat(accum) + parseFloat(item.amount), 0);
+        if (response.data.wagers.length > 0) {
+            onSuccess(response.data.wagers);
+        } else {
+            onError();
+        }
+    };
 
-  const getBasePlayerBalance = () => playerData.players.find(x => x._id === sessionInfo.playerId).currentFunds;
+    const aggregateWagerValues = (wagers) => wagers.reduce((accum, item) => parseFloat(accum) + parseFloat(item.amount), 0);
+    const getBasePlayerBalance = () => playerData.players.find(x => x._id === sessionInfo.playerId).currentFunds;
 
-  useEffect(() => {
-    if (playerData.players !== undefined)
-      setCurrentPlayerBalance(getBasePlayerBalance());
-  }, [isPlayerDataLoading]);
+    useEffect(() => {
+        if (playerData.players !== undefined) {
+            setCurrentPlayerBalance(getBasePlayerBalance());
+        }
+    }, [isPlayerDataLoading]);
 
+    return (
+        <Body>
+            <ToastContainer
+                ref={ref => toastrContainer = ref}
+                className="toast-top-right"
+            />
 
+            <div className={css.pageContainer}>
+                {(isLoading && isPlayerDataLoading)
+                    ? (
+                        <div>Loading.....Please Wait</div>
+                    )
+                    : (apiError.error)
+                        ? (
+                            <div>
+                                <p>There was an error retrieving the data for this race!</p>
 
-  return (
-    <Body>
-      <div className={css.pageContainer}>
-        {isLoading && isPlayerDataLoading ? (
-          <div>Loading.....Please Wait</div>
-        ) : apiError.error ? (
-          <div>
-            <p>There was an error retrieving the data for this race!</p>
-            {apiError.messages
-              .filter(message => message)
-              .map((message, idx) => (
-                <p key={idx}>{message}</p>
-              ))}
-          </div>
-        ) : (
-              <>
-                <h5>{raceDayData.name} - {raceDayData.races.length} RACES</h5>
-                <h1>Game PIN: {raceDayData.pin}</h1>
-                <div className={css.balance}>Balance: £{currentPlayerBalance}</div>
-                <div className={css.raceCardContainer}>
-                  <WagerCard raceCard={getNextRace()} callback={handleBetsChanged}>
+                                {apiError.messages
+                                    .filter(message => message)
+                                    .map((message, idx) => (
+                                        <p key={idx}>{message}</p>
+                                    ))
+                                }
+                            </div>
+                        ) : (
+                            <div>
+                                <h5>{raceDayData.name} - {raceDayData.races.length} RACES</h5>
 
-                  </WagerCard>
+                                <h1>Game PIN: {raceDayData.pin}</h1>
 
-                </div>
-                <div className={css.buttonContainer}>
-                  <Card className={classes.root} onClick={handlePlaceBet}>
-                    <div>{placeBetText}</div>
-                  </Card>
-                </div>
-              </>
-            )}
-      </div>
-    </Body>
-  );
+                                <div className={css.balance}>Balance: £{currentPlayerBalance}</div>
+
+                                <div className={css.raceCardContainer}>
+                                    <WagerCard raceCard={getNextRace()} callback={handleBetsChanged} />
+                                </div>
+
+                                <div className={css.buttonContainer}>
+                                    <Card
+                                        className={classes.root}
+                                        onClick={handlePlaceBet}
+                                        ref={toastrContainer}
+                                    >
+                                        <div>{placeBetText}</div>
+                                    </Card>
+                                </div>
+                            </div>
+                        )
+                }
+            </div>
+        </Body>
+    );
 };
 
 export default PlayerLobby;
