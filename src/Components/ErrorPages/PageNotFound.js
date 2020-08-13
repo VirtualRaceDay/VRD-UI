703cb8049ce8623132f2cb0387aeed3e0a9476d8
Phil Steer <pd@incb.us>
2020-08-13 12:42:44 +0100

    Tidy up, re-indent ErrorPages folder files

--- src/Components/ErrorPages/PageNotFound.js
+++ src/Components/ErrorPages/PageNotFound.js
@@ -1,12 +1,13 @@
 import React from 'react';
+
 import Body from '../Body/Body';
 
 const PageNotFound = () => {
-  return (
-    <Body>
-      <div>PageNotFound</div>
-    </Body>
-  );
+    return (
+        <Body>
+            <h2>Page could not be found</h2>
+        </Body>
+    );
 };
 
 export default PageNotFound;
