3154e12b2f3d61d747a1b5234784ca43993f22ef
Phil Steer <pd@incb.us>
2020-08-14 09:36:54 +0100

    Tidy up, re-indent LandingPage folder files

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -2,48 +2,50 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import { makeStyles } from '@material-ui/core/styles';
+
 import css from './LandingPage.module.css';
 import Body from '../Body/Body';
 
 const useStyles = makeStyles({
-  root: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 200,
-    height: 200,
-    border: '1px solid #00000026',
-    '&:hover, &:focus': {
-      backgroundColor: '#fafafa',
-      cursor: 'pointer',
+    root: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 200,
+        height: 200,
+        border: '1px solid #00000026',
+        '&:hover, &:focus': {
+            backgroundColor: '#fafafa',
+            cursor: 'pointer',
+        },
     },
-  },
 });
 
 const LandingPage = () => {
-  const classes = useStyles();
-  const history = useHistory();
+    const classes = useStyles();
+    const history = useHistory();
 
-  const handleOnHostClick = () => {
-    history.push('/Host');
-  };
+    const handleOnHostClick = () => {
+        history.push('/Host');
+    };
 
-  const handleOnJoinClick = () => {
-    history.push('/Join');
-  };
+    const handleOnJoinClick = () => {
+        history.push('/Join');
+    };
 
-  return (
-    <Body>
-      <div className={css.buttonContainer}>
-        <Card className={classes.root} onClick={handleOnHostClick}>
-          <div>Host</div>
-        </Card>
-        <Card className={classes.root} onClick={handleOnJoinClick}>
-          <div>Join</div>
-        </Card>
-      </div>
-    </Body>
-  );
+    return (
+        <Body>
+            <div className={css.buttonContainer}>
+                <Card className={classes.root} onClick={handleOnHostClick}>
+                    <div>Host</div>
+                </Card>
+
+                <Card className={classes.root} onClick={handleOnJoinClick}>
+                    <div>Join</div>
+                </Card>
+            </div>
+        </Body>
+    );
 };
 
 export default LandingPage;
