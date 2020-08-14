3c584e1b9121b427f688bb905f8a6b9c931292b5
Phil Steer <pd@incb.us>
2020-08-14 09:32:08 +0100

    Tidy up, re-indent JoinPage folder files

--- src/Components/JoinPage/JoinPage.js
+++ src/Components/JoinPage/JoinPage.js
@@ -1,94 +1,99 @@
 import React, { useState } from 'react';
 import Body from '../Body/Body';
 import { useHistory } from 'react-router-dom';
-
 import Card from '@material-ui/core/Card';
 import TextField from '@material-ui/core/TextField';
-
 import { makeStyles } from '@material-ui/core/styles';
+
 import css from './JoinPage.module.css';
 import { postToApi } from "../../utils/apiLayer";
 
 const useStyles = makeStyles({
-  root: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 120,
-    height: 80,
-    border: '1px solid #00000026',
-    '&:hover, &:focus': {
-      backgroundColor: '#fafafa',
-      cursor: 'pointer',
+    root: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 120,
+        height: 80,
+        border: '1px solid #00000026',
+        '&:hover, &:focus': {
+            backgroundColor: '#fafafa',
+            cursor: 'pointer',
+        },
     },
-  },
 });
 
 const JoinPage = () => {
-  const classes = useStyles();
-  const history = useHistory();
+    const classes = useStyles();
+    const history = useHistory();
+    const [pin, setPin] = useState('');
+    const [nickname, setNickname] = useState('');
 
-  const [pin, setPin] = useState('');
-  const [nickname, setNickname] = useState('');
+    const handleOnJoinClick = async () => {
+        const response = await postToApi('/player', { pin, nickname });
 
-  const handleOnJoinClick = async () => {
-    const response = await postToApi('/player', { pin, nickname });
-    if (!response.error) {
-      const { data } = response;
-      history.push({
-        pathname: '/PlayerLobby',
-        state: {
-          sessionInfo: {
-            playerId: data.playerId,
-            raceDayId: data.raceDayId,
-            nickname: nickname
-          }
-        },
-      });
-    } else {
-      alert(`There was an error joining the game!\n${response.error}`);
-    }
-  };
+        if (!response.error) {
+            const { data } = response;
 
-  const handlePinChange = (event) => {
-    setPin(event.target.value);
-  };
+            history.push({
+                pathname: '/PlayerLobby',
+                state: {
+                    sessionInfo: {
+                        playerId: data.playerId,
+                        raceDayId: data.raceDayId,
+                        nickname: nickname
+                    }
+                },
+            });
+        } else {
+            alert(`There was an error joining the game!\n${response.error}`);
+        }
+    };
 
-  const handleNicknameChange = (event) => {
-    setNickname(event.target.value);
-  };
+    const handlePinChange = (event) => {
+        setPin(event.target.value);
+    };
 
-  return (
-    <Body>
-      <form className={css.pageContainer} noValidate autoComplete="off">
-        <div className={css.joinDetailsContainer}>
-          <TextField
-            style={{ width: '50%' }}
-            id="standard-basic"
-            label="Enter Game PIN"
-            value={pin}
-            onChange={handlePinChange}
-          />
-          <TextField
-            style={{ width: '50%' }}
-            id="standard-basic"
-            label="Enter Nickname"
-            value={nickname}
-            onChange={handleNicknameChange}
-          />
-        </div>
-        <div className={css.joinButtonContainer}>
-          <Card
-            type="submit"
-            className={classes.root}
-            onClick={handleOnJoinClick}
-          >
-            <div>Join</div>
-          </Card>
-        </div>
-      </form>
-    </Body>
-  );
+    const handleNicknameChange = (event) => {
+        setNickname(event.target.value);
+    };
+
+    return (
+        <Body>
+            <form
+                className={css.pageContainer}
+                noValidate
+                autoComplete="off"
+            >
+                <div className={css.joinDetailsContainer}>
+                    <TextField
+                        style={{ width: '50%' }}
+                        id="standard-basic"
+                        label="Enter Game PIN"
+                        value={pin}
+                        onChange={handlePinChange}
+                    />
+                    <TextField
+                        style={{ width: '50%' }}
+                        id="standard-basic"
+                        label="Enter Nickname"
+                        value={nickname}
+                        onChange={handleNicknameChange}
+                    />
+                </div>
+
+                <div className={css.joinButtonContainer}>
+                    <Card
+                        type="submit"
+                        className={classes.root}
+                        onClick={handleOnJoinClick}
+                    >
+                        <div>Join</div>
+                    </Card>
+                </div>
+            </form>
+        </Body>
+    );
 };
 
 export default JoinPage;
