15a8259695de4cddd2177030e511366d6854adf4
Phil Steer <pd@incb.us>
2020-08-14 09:54:39 +0100

    Tidy up, re-indent PLayerIdentifier folder files

--- src/Components/PlayerIdentifier/PlayerIdentifier.js
+++ src/Components/PlayerIdentifier/PlayerIdentifier.js
@@ -1,18 +1,20 @@
 import React from 'react';
+
 import css from './PlayerIdentifier.module.css';
 import image from '../../logo512.png';
 
 const PlayerIdentifier = ({ name }) => {
-  return (
-    <>
-      <div className={css.playerCard}>
-        <div className={css.iconContainer}>
-          <img className={css.icon} src={image} alt="Player Avatar" />
-        </div>
-        <div className={css.playerName}>{name}</div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={css.playerCard}>
+                <div className={css.iconContainer}>
+                    <img className={css.icon} src={image} alt="Player Avatar" />
+                </div>
+
+                <div className={css.playerName}>{name}</div>
+            </div>
+        </>
+    );
 };
 
 export default PlayerIdentifier;
